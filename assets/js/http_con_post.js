a94db204107a32dee3a27bdba87be7d13b19853c
CPrz21 <34002692+CPrz21@users.noreply.github.com>
2018-04-17 12:14:21 -0600

    Create http_con_post.js

---
+++ assets/js/http_con_post.js
@@ -0,0 +1,18 @@
+// var myURL='http://192.0.0.8/OVST_API/api/getEstadoCuentaVale';
+  // var _data ={tipo:2, idtipo_soc:'V', cliente_pos:'011164142', vale:'91727410213', idestado:0, token:'0x892365285740bbe864b583c699f32f64240ad48b76609292f0ef16b735b972ff', parametro:'-'};
+
+//   $http({
+//         method : "POST",
+//         url : myURL,
+//         data:$httpParamSerializerJQLike(_data),
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded'
+//         }
+//
+//     }).then(function mySuccess(data) {
+//
+//       console.log(data);
+//       // $scope.posts=data.data.data;
+//     }, function myError(err) {
+//       console.log(err);
+// });
